05c479f481dd537487a8f932605473962e1e24db
Deep Jawale <deepjawale801@gmail.com>
2022-10-10 22:56:42 +0530

    Included header files

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
 using namespace std;
+
+//Including all hader files needed
+#include <hospital.hpp>
+#include <location.hpp>
+#include <user.hpp>
+#include <timing.hpp>
+#include <contact.hpp>
+#include <dept.hpp>
+
+
 int main()
 {
     
