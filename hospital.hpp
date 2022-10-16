7de850c24de36c08f82501d02ec5ae2ad7b52dfe
Deep Jawale <deepjawale801@gmail.com>
2022-10-17 02:41:29 +0530

    Added implementation of hospital class

--- hospital.hpp
+++ hospital.hpp
@@ -16,12 +16,45 @@ class hospital
     }
 
     public:
+    //Fields
     string name;
     string organization_name;
     location address;
     contact contact_details;
     timing opd_timing;
     int num_doc;
+
+    //Member Functions or Methods
+    void get_contact()
+    {
+        contact_details.show();
+    }
+
+    void get_details()
+    {
+        cout<<"Name: "<<name<<endl;
+        cout<<"Organisation: "<<organization_name<<endl;
+        // address.show();
+        // contact_details.show();
+        // opd_timing.show();
+        cout<<"No, of doctors: "<<num_doc<<endl;
+    }
+
+    void set_name(string s)
+    {
+        this->name = s;
+    }
+
+    void set_org(string s)
+    {
+        this->organization_name = s;
+    }
+
+    void set_num_doc()
+    {
+        cout<<"Enter number of doctors: ";
+        cin>>this->num_doc;
+    }
 
     protected:
 
