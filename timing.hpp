66e8fca2c881d81b1ef38cd70aa1b1938ec3d538
ManavJethva <110109336+ManavJethva@users.noreply.github.com>
2022-10-21 17:06:08 +0530

    Update timing.hpp

--- timing.hpp
+++ timing.hpp
@@ -1,7 +1,58 @@
+#include<iostream>
+using namespace std;
+#include<vector>
 
+class timing{
+    public:
+    vector<string> time_slots;
+    vector<string> day_avail;
 
+    void view_slots(){
+        cout<<" The Time slots are: "<<endl;
+        for (int i = 0; i < time_slots.size(); i++)
+        {
+            cout<<time_slots[i]<<" ";
+        }
 
-class timing
-{
+        cout<<" The available days are: "<<endl;
+        for (int i = 0; i < day_avail.size(); i++)
+        {
+            cout<<day_avail[i]<<" ";
+        }
+        
+    }
+
+    void update_slots(){
+        cout<<"If you want to add available day  press u else q "<<endl;
+        char u;
+        cin>>u;
+        string new_day;
+        while(u=='u'){
+            cout<<"The day to be added "<<endl;
+            cin>>new_day;
+            time_slots.push_back(new_day);
+             cout<<"If you want to add available day  press u else q "<<endl;
+            cin>>u;
+        } 
+
+        cout<<"If you want to add a time slot press u else q "<<endl;
+        char u;
+        cin>>u;
+        string new_slot;
+        while(u=='u'){
+            cout<<"The time slot to be added "<<endl;
+            cin>>new_slot;
+            time_slots.push_back(new_slot);
+            cout<<"If you want to add a time slot press u else q "<<endl;
+            cin>>u;
+        } 
+
+         
+    }
+     
 
 };
+
+
+
+
