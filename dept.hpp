c9077bd6829938254ce35243309f5903519e68ac
ManavJethva <110109336+ManavJethva@users.noreply.github.com>
2022-10-21 17:04:17 +0530

    Update dept.hpp

--- dept.hpp
+++ dept.hpp
@@ -1,5 +1,28 @@
+#include<iostream>
+using namespace std;
+
+#include<timing.hpp>
 
 class dept
-{
+{   
+    public:
+    string department_name;
+    int num_doc;
+    timing timings;
 
+    //functions
+    int get_avail(){
+         cout<<"The department name is: "<<department_name <<endl;
+         cout<<"The number of available doctors are: "<<num_doc<<endl;
+         cout<<"The available timings are: "<<timings; 
+    }
+    private:
+    void set_avail(){
+        cout<<"Enter the department name: "<<endl;
+        cin>>department_name;
+        cout<<"The number of doctors available for "<<department_name<<" are "<<num_doc<<endl;
+        timings.update_slots();
+    }
+
+    
 };
